41c08539208ec87a86eaa068cc1af64445fe477d
Sushil Kumar Gupta <sgupta7747@altimetrik.com>
2023-11-25 17:49:09 +0530

    1st commit

---
+++ akasha/src/test/java/retro/AwsOperation.java
@@ -0,0 +1,18 @@
+package retro;
+
+import org.testng.annotations.Test;
+
+public class AwsOperation {
+    @Test
+    public void operation1()
+    {
+        System.out.println("this is operation 1");
+
+    }
+
+    @Test
+    public void operation2()
+    {
+        System.out.println("this is operation 2");
+    }
+}
